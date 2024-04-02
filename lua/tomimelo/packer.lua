b939f49ce3e368dbaaccffe23b68213b615f9df3
Tomas Melone <tomas.melone@es.stroeer-labs.com>
2024-04-02 13:48:53 +0200

    some changes

--- lua/tomimelo/packer.lua
+++ lua/tomimelo/packer.lua
@@ -1,52 +1,74 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	use({ 'rose-pine/neovim', as = 'rose-pine', config = function()
-		require('rose-pine').setup({
-			disable_italics = true,
-			highlight_groups = {
-				Comment = { italic = true }
-			}
-		})
-		vim.cmd('colorscheme rose-pine')
-	end
+    use("folke/tokyonight.nvim")
 
-	})
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            require('rose-pine').setup({
+                disable_italics = true,
+                highlight_groups = {
+                    Comment = { italic = true }
+                }
+            })
+            vim.cmd('colorscheme rose-pine')
+        end
 
-	use "nvim-lua/plenary.nvim"
-	use {
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		requires = { {"nvim-lua/plenary.nvim"} }
-	}
+    })
+    use({
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup {
+                icons = false,
+            }
+        end
+    })
 
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+    use "nvim-lua/plenary.nvim"
+    use {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
 
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+        }
+    }
+
+	use("eandrju/cellular-automaton.nvim")
 end)
-
