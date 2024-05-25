076b953d659f9b41dce19f82fe1dd0e740d11aed
Tomas Melone <tomas.melone@es.stroeer-labs.com>
2024-05-25 11:31:24 +0200

    add oil config

---
+++ lua/tomimelo/lazy/oil.lua
@@ -0,0 +1,10 @@
+return {
+    'stevearc/oil.nvim',
+    opts = {
+        keymaps = {
+            ["<C-p>"] = false,
+        },
+    },
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+}
